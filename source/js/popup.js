ea869c42f724a3b2b344d8cbce3c5a015ef5b4a5
kolyas238 <kolyas238@gmail.com>
2020-04-30 18:05:10 +0300

    вносит правки в popup.js

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,11 +1,15 @@
-var cart = document.querySelector('.cart');
+var cart = document.querySelectorAll('.cart');
 var wrap = document.querySelector('.popup-wrap');
 
+wrap.classList.remove("nojs");
 wrap.classList.add("popup_hide");
-cart.addEventListener("click", function(evt) {
-evt.preventDefault();
-wrap.classList.toggle("popup_hide");
+
+for (var i = 0; i < cart.length; i++) {
+  cart[i].addEventListener("click", function(evt) {
+  evt.preventDefault();
+  wrap.classList.remove("popup_hide");
 });
+}
 
 wrap.onclick = function(close) {
   if (close.target == wrap) {
