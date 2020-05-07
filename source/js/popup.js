b15febed2e1ae28e719472d7943c4deafe2b9d47
kolyas238 <kolyas238@gmail.com>
2020-05-08 01:32:57 +0300

    Вносит правки

--- source/js/popup.js
+++ source/js/popup.js
@@ -2,19 +2,19 @@ var cart = document.querySelectorAll('.cart-js');
 var wrap = document.querySelector('.popup-wrap');
 
 wrap.classList.remove("nojs");
-wrap.classList.add("popup_hide");
+wrap.classList.add("popup-hide");
 
 for (var i = 0; i < cart.length; i++) {
   cart[i].addEventListener("click", function(evt) {
     evt.preventDefault();
-    wrap.classList.remove("popup_hide");
+    wrap.classList.remove("popup-hide");
     wrap.classList.remove("popup-wrap--position");
   });
 }
 
 wrap.onclick = function(close) {
   if (close.target == wrap) {
-    wrap.classList.toggle("popup_hide");
+    wrap.classList.toggle("popup-hide");
     wrap.classList.toggle("popup-wrap--position");
   }
 }
@@ -23,6 +23,6 @@ document.body.addEventListener('keyup', function(e) {
   var key = e.keyCode;
 
   if (key == 27) {
-    document.querySelector('.popup-wrap').classList.add('popup_hide');
+    document.querySelector('.popup-wrap').classList.add('popup-hide');
   };
 }, false);
